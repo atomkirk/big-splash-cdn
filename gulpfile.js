08022a6fa0ef14f717736c6d6b366412e76ad23f
Adam Kirk <atomkirk@gmail.com>
2015-11-06 14:01:26 -0700

    configuring deploy to s3

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
+var fs = require('fs');
 var s3 = require("gulp-s3");
+var gzip = require("gulp-gzip");
 
 gulp.task('default', function() {
+
 });
 
 gulp.task('deploy', function() {
+
+  var options = { gzippedOnly: true };
+
   aws = JSON.parse(fs.readFileSync('./aws.json'));
+
   gulp.src('./dist/**')
-      .pipe(s3(aws));
+    .pipe(gzip())
+    .pipe(s3(aws, options));
+
 });
